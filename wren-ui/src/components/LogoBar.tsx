445c50bd02e2685764073920db39c3d00bf4524f
Erick Haendel <erick.haendel@findup.com.br>
2025-04-14 14:48:00 -0300

    feat: :art: logo change

--- wren-ui/src/components/LogoBar.tsx
+++ wren-ui/src/components/LogoBar.tsx
@@ -3,8 +3,10 @@ import Image from 'next/image';
 export default function LogoBar() {
   return (
     <Image
-      src="/images/logo-white-with-text.svg"
-      alt="Wren AI"
+      src="/images/logo-white.png"
+      alt="FindUP Analise"
+      className="w-full"
+      style={{ width: '100%' }}
       width={125}
       height={30}
     />
